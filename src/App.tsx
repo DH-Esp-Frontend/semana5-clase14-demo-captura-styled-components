b171bdb6c591aa226c473bdb105ce8975f022f88
Alexis <alexislazzuri@gmail.com>
2022-05-05 10:48:27 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Button from "./components/Button";
+import Input from "./components/Input";
+import { useInput } from "./hooks/useInput";
+import "./App.css";
 
 function App() {
+  const [isActive, setIsActive] = useState(false);
+  const { value: username, onChange: onChangeUsername } = useInput("");
+  const { value: password, onChange: onChangePassword } = useInput("");
+
+  const handleClick = () => {
+    setIsActive(false);
+    setTimeout(() => {
+      alert("Bienvenido " + username + "!");
+      window.location.reload();
+    }, 1000);
+  };
+
+  useEffect(() => {
+    if (
+      username.match(/^[a-zA-Z0-9]{3,}$/) &&
+      password.match(/^[a-zA-Z0-9]{3,}$/)
+    ) {
+      setIsActive(true);
+    } else {
+      setIsActive(false);
+    }
+  }, [username, password]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Iniciar Sesión</h1>
+      <Input
+        label="Nombre de Usuario"
+        placeholder="Ingrese su nombre de usuario"
+        name="username"
+        value={username}
+        onInputChange={onChangeUsername}
+      />
+      <Input
+        label="Contraseña"
+        placeholder="Ingrese su contraseña"
+        name="password"
+        value={password}
+        onInputChange={onChangePassword}
+        type="password"
+      />
+      <Button isActive={isActive} onClick={handleClick} disabled={!isActive}>
+        Iniciar Sesión
+      </Button>
     </div>
   );
 }
